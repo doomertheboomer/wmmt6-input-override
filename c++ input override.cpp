ed2d30d86a45a30477023ab3105e87cf8b2e4b99
Sam Sam Sam <hackerdoomere@gmail.com>
2021-06-22 14:19:43 +0700

    program now works as intended, although cpu intensive. will search for solution

--- c++ input override.cpp
+++ c++ input override.cpp
@@ -7,6 +7,8 @@
 #include <SFML/Window/Joystick.hpp>
 #include "proc.h"
 
+uintptr_t brakeAddr, gasAddr;
+
 int main()
 {
 	
@@ -23,12 +25,18 @@ int main()
 	//resolve base addr
 	uintptr_t pCarStruct = (imageBase + 0x1E5B5B0);
 
-	//set rolling start
+	//set rolling start address
 	uintptr_t rollingAddr = (imageBase + 0x1E5B8FC);
 	float rollingTimer = 0;
 
-	//initialize gas and brake to minimize errors
-	uintptr_t gasAddr = 0, brakeAddr = 0; 
+	//resolve override patch addresses
+	uintptr_t patch1Addr = (imageBase + 0xCD1AA);
+	uintptr_t patch2Addr = (imageBase + 0xCD1B3);
+
+	//patch1 and 2 values
+	unsigned char stock1[6]{ 0x89, 0x81, 0xAC, 0x06, 0x00, 0x00 };
+	unsigned char stock2[6]{ 0x89, 0x81, 0xB0, 0x06, 0x00, 0x00 };
+	unsigned char patched[6]{ 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
 
 	int x = 1;
 	do
@@ -43,6 +51,18 @@ int main()
 		ReadProcessMemory(hProcess, (DWORD*)rollingAddr, &rollingTimer, sizeof(rollingTimer), nullptr);
 		if (rollingTimer != 0)
 		{
+			//remove override
+			WriteProcessMemory(hProcess, (DWORD*)patch1Addr, &stock1, sizeof(stock1), nullptr);
+			WriteProcessMemory(hProcess, (DWORD*)patch2Addr, &stock2, sizeof(stock2), nullptr);
+
+			std::cout << "Gas found at 0x" << std::hex << gasAddr << " Brake found at 0x" << std::hex << brakeAddr<< std::endl;
+		}
+		else
+		{
+			//start override
+			WriteProcessMemory(hProcess, (DWORD*)patch1Addr, &patched, sizeof(patched), nullptr);
+			WriteProcessMemory(hProcess, (DWORD*)patch2Addr, &patched, sizeof(patched), nullptr);
+
 			//resolve gas pointer
 			std::vector<unsigned int> gasOffsets = { 0xB10 };
 			uintptr_t gasAddr = findDMAAddy(hProcess, pCarStruct, gasOffsets);
@@ -51,18 +71,14 @@ int main()
 			std::vector<unsigned int> brakeOffsets = { 0xB14 };
 			uintptr_t brakeAddr = findDMAAddy(hProcess, pCarStruct, brakeOffsets);
 
-			std::cout << "Gas found at 0x" << std::hex << gasAddr << " Brake found at 0x" << std::hex << brakeAddr<< std::endl;
-		}
-		else
-		{
 			sf::Joystick::update();
-			float left = (sf::Joystick::getAxisPosition(0, sf::Joystick::U) + 100) / 200;
-			float right = (sf::Joystick::getAxisPosition(0, sf::Joystick::V) + 100) / 200;
+			float left = ((sf::Joystick::getAxisPosition(0, sf::Joystick::U) + 100) / 200);
+			float right = ((sf::Joystick::getAxisPosition(0, sf::Joystick::V) + 100) / 200);
 
 			//write to gas
 			WriteProcessMemory(hProcess, (DWORD*)gasAddr, &right, sizeof(right), nullptr);
 			WriteProcessMemory(hProcess, (DWORD*)brakeAddr, &left, sizeof(left), nullptr);
-			std::cout << "Left Trigger: " << (std::to_string(left)) << "| Right Trigger: " << (std::to_string(right)) << (std::endl);
+			std::cout << "| Brake at 0x" << std::hex << brakeAddr << ": " << (std::to_string(left)) << "| Gas at 0x" << std::hex << gasAddr << ": " << (std::to_string(right)) << (std::endl);
 		}
 	} while (x = 1); //monitors trigger of dualshock 4
 
