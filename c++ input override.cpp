b3a5495a127e36de90a0c1d4bc8f12c494de95ba
Sam Sam Sam <hackerdoomere@gmail.com>
2021-06-22 13:41:22 +0700

    begin work on overriding

--- c++ input override.cpp
+++ c++ input override.cpp
@@ -7,9 +7,6 @@
 #include <SFML/Window/Joystick.hpp>
 #include "proc.h"
 
-
-
-
 int main()
 {
 	
@@ -23,28 +20,55 @@ int main()
 	HANDLE hProcess = 0;
 	hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
 
-	int x = 1;
+	//resolve base addr
+	uintptr_t pCarStruct = (imageBase + 0x1E5B5B0);
 
+	//set rolling start
+	uintptr_t rollingAddr = (imageBase + 0x1E5B8FC);
+	float rollingTimer = 0;
+
+	//initialize gas and brake to minimize errors
+	uintptr_t gasAddr = 0, brakeAddr = 0; 
+
+	int x = 1;
 	do
 	{
+		/* debug stuff
 		sf::Joystick::update();
-		float left = sf::Joystick::getAxisPosition(0, sf::Joystick::U);
-		float right = sf::Joystick::getAxisPosition(0, sf::Joystick::V);
+		float left = (sf::Joystick::getAxisPosition(0, sf::Joystick::U) + 100) / 200;
+		float right = (sf::Joystick::getAxisPosition(0, sf::Joystick::V) + 100) / 200;
 		std::cout << "Left Trigger: " << (std::to_string(left)) << "| Right Trigger: " << (std::to_string(right)) << (std::endl);
+		*/
+
+		ReadProcessMemory(hProcess, (DWORD*)rollingAddr, &rollingTimer, sizeof(rollingTimer), nullptr);
+		if (rollingTimer != 0)
+		{
+			//resolve gas pointer
+			std::vector<unsigned int> gasOffsets = { 0xB10 };
+			uintptr_t gasAddr = findDMAAddy(hProcess, pCarStruct, gasOffsets);
+
+			//resolve brake pointer
+			std::vector<unsigned int> brakeOffsets = { 0xB14 };
+			uintptr_t brakeAddr = findDMAAddy(hProcess, pCarStruct, brakeOffsets);
+
+			std::cout << "Gas found at 0x" << std::hex << gasAddr << " Brake found at 0x" << std::hex << brakeAddr<< std::endl;
+		}
+		else
+		{
+			sf::Joystick::update();
+			float left = (sf::Joystick::getAxisPosition(0, sf::Joystick::U) + 100) / 200;
+			float right = (sf::Joystick::getAxisPosition(0, sf::Joystick::V) + 100) / 200;
+
+			//write to gas
+			WriteProcessMemory(hProcess, (DWORD*)gasAddr, &right, sizeof(right), nullptr);
+			WriteProcessMemory(hProcess, (DWORD*)brakeAddr, &left, sizeof(left), nullptr);
+			std::cout << "Left Trigger: " << (std::to_string(left)) << "| Right Trigger: " << (std::to_string(right)) << (std::endl);
+		}
 	} while (x = 1); //monitors trigger of dualshock 4
 
 
 	
 	/* leftovers
-	
-	//resolve base addr
-	uintptr_t dynamicPtrBaseAddr = (imageBase + 0x1E183F8);
-
-	std::cout << "DynamicPtrBaseAddr = " << "0x" << std::hex << dynamicPtrBaseAddr << std::endl;
-
-	//resolve coin pointer
-	std::vector<unsigned int> coinOffsets = { 0xA8 };
-	uintptr_t coinAddr = findDMAAddy(hProcess, dynamicPtrBaseAddr, coinOffsets);
 
 	std::cout << "coinAddr = " << "0x" << std::hex << coinAddr << std::endl;
 
@@ -52,10 +76,6 @@ int main()
 	int coinValue = 0;
 	ReadProcessMemory(hProcess, (DWORD*)coinAddr, &coinValue, sizeof(coinValue), nullptr);
 	std::cout << "Current coin = " << std::dec << coinValue << std::endl;
-
-	//write to coin
-	int newCoin = 69;
-	WriteProcessMemory(hProcess, (DWORD*)coinAddr, &newCoin, sizeof(newCoin), nullptr);
 
 	//read coin value
 	ReadProcessMemory(hProcess, (DWORD*)coinAddr, &coinValue, sizeof(coinValue), nullptr);
